e2fc56d5d7e3ca52162f86bf2e95f4e376394f2e
will mcbrien <williamjamesmcbrien@gmail.com>
2018-10-15 16:53:28 +0100

    Refactor to ask user for list of student names

--- directory.rb
+++ directory.rb
@@ -1,16 +1,18 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Coleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Fready Kruger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :novembe}
-]
+def input_statements
+  puts "Please enter the names of the students"
+  puts "To finishm just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -27,6 +29,7 @@ def print_footer(names)
   puts "Overall,  we have #{names.count} great students"
 end
 
+students = input_statements
 print_header
 print(students)
 print_footer(students)
