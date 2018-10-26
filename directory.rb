e222c1a8e87d14e88e4f74a59adcd46b85c624b2
will mcbrien <williamjamesmcbrien@gmail.com>
2018-10-26 13:36:27 +0100

    Refactor, step-10

--- directory.rb
+++ directory.rb
@@ -1,42 +1,46 @@
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
+@students = []
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
-      exit # this will cause the program to terminate
+      exit
     else
-      puts "I don't know what you meant, try again"
-    end
+      puts "I don't know what you mean, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish just hit return twice"
-
-  students = []
-
   name = gets.chomp
-
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
 end
 
 def print_header
@@ -44,16 +48,16 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  if !students.empty?
-    students.each_with_index  do |student, index|
+def print_students_list
+  if !@students.empty?
+    @students.each_with_index  do |student, index|
       puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
 end
 
-def print_footer(names)
-  puts "Overall,  we have #{names.count} great students"
+def print_footer
+  puts "Overall,  we have #{@students.count} great students"
 end
 
 interactive_menu
